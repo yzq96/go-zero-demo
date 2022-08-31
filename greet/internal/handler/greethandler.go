31ec51636d121170b848150ed86f78abadc40fa1
yzq <yzq@qq.com>
2022-08-31 18:11:45 +0800

    first commit

---
+++ greet/internal/handler/greethandler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+
+	"gozero/greet/internal/logic"
+	"gozero/greet/internal/svc"
+	"gozero/greet/internal/types"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func GreetHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewGreetLogic(r.Context(), svcCtx)
+		resp, err := l.Greet(&req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
---
+++ greet/internal/logic/greetlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"gozero/greet/internal/svc"
+	"gozero/greet/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GreetLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewGreetLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GreetLogic {
+	return &GreetLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *GreetLogic) Greet(req *types.Request) (*types.Response, error) {
+	return &types.Response{
+		Message: "Hello go-zero",
+	}, nil
+}
